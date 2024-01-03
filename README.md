b2ae162fff6732eb04670c87f3cf2249bdf1d567
Andrey Ivanov <ivanov.andrey.cc@yandex.ru>
2024-01-03 20:13:18 +0300

    Two sum solution

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # leetcode
+
+Решение задач leetcode
---
+++ src/main/kotlin/ru/leetcode/array/TwoSum.kt
@@ -0,0 +1,20 @@
+package ru.leetcode.array
+
+fun main() {
+    println(TwoSum().twoSum(intArrayOf(2, 7, 11, 15), 9).joinToString(","))
+}
+
+class TwoSum {
+
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val indexByValue = mutableMapOf<Int, Int>()
+        nums.forEachIndexed { index, num ->
+            if (indexByValue.contains(target - num)) {
+                return intArrayOf(indexByValue[target - num]!!, index)
+            } else {
+                indexByValue[num] = index
+            }
+        }
+        return intArrayOf()
+    }
+}
